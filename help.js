13f7e3dbf6c7ae0b5b916afccc21866c6acbf294
Silas-star <kauavefago20@gmail.com>
2021-02-06 08:54:11 -0300

    First commit

---
+++ help.js
@@ -0,0 +1,130 @@
+
+const help = (prefix) => { 
+	return `                 
+┏━━❉ *Bot ρєrsσнαr∂* ❉━━┓
+┣⊱
+┣⊱Prefix; 「${prefix}」
+┣⊱Dono;Silas
+┣⊱contato;wa.me/+5548998006626
+┣⊱Versão;Beta
+┣⊱Nota:essa é a versão beta não está 100%traduzida então daqui uns dias eu irei atualizar
+┣⊱
+┣━━❉ *Comandos RPG* ❉━━
+┣⊱
+┣⊱${prefix}introdução
+┣⊱- Mostra alguns detalhes do RPG em geral.
+┣⊱
+┣⊱${prefix}sistema
+┣⊱- Mostra o sistema do RPG.
+┣⊱
+┣⊱${prefix}site
+┣⊱- Mostra o site do RPG.
+┣⊱
+┣⊱${prefix}historia
+┣⊱- Mostra a história do mundo Persohard.
+┣⊱
+┣⊱${prefix}ficha
+┣⊱- Envia a ficha de criação.
+┣⊱
+┣⊱${prefix}nerf
+┣⊱- Envia os poderes nerfados/bloqueados.
+┣⊱
+┣⊱${prefix}link
+┣⊱- Envia o link dos grupos (on, off e avisos)
+┣⊱
+┣⊱${prefix}spam
+┣⊱- Envia o spam do RPG.
+┣━━❉ *Sobre o Bot* ❉━━
+┣⊱ *${prefix}owner*
+┣⊱ *${prefix}donasi*
+┣⊱ *${prefix}info*
+┣⊱ *${prefix}botstat*
+┣⊱ *${prefix}bug*
+┣━━❀ *Fazer* ❀━━
+┣⊱ *${prefix}ocr*
+┣⊱ *${prefix}sticker*
+┣⊱ *${prefix}tts*
+┣⊱ *${prefix}toimg*
+┣⊱ *${prefix}phlogo*
+┣⊱ *${prefix}quotemaker*
+┣⊱ *${prefix}ninjalogo*
+┣⊱ *${prefix}glitch*
+┣⊱ *${prefix}text3d*
+┣━━❀ *Kerang ajaib* ❀━
+┣⊱ *${prefix}kapankah*
+┣⊱ *${prefix}apakah*
+┣⊱ *${prefix}bisakah*
+┣⊱ *${prefix}rate*
+┣━━❀ *Fun* ❀━━━━
+┣⊱ *${prefix}meme*
+┣⊱ *${prefix}memeindo*
+┣⊱ *${prefix}hilih*
+┣⊱ *${prefix}fitnah*
+┣⊱ *${prefix}truth*
+┣⊱ *${prefix}dare*
+┣⊱ *${prefix}bucin*
+┣⊱ *${prefix}persengay*
+┣━━❀ *Anime* ❀━━
+┣⊱ *${prefix}loli*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}anime*
+┣⊱ *${prefix}randomanime*
+┣⊱ *${prefix}randomhentai*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}nsfwblowjob*
+┣⊱ *${prefix}nsfwneko*
+┣⊱ *${prefix}nsfwtrap*
+┣━━❀ *Media* ❀━━
+┣⊱ *${prefix}tiktokstalk*
+┣⊱ *${prefix}ytmp4*
+┣━━❀ *Outros* ❀━━
+┣⊱ *${prefix}ping*
+┣⊱ *${prefix}shorturl*
+┣⊱ *${prefix}fototiktok*
+┣⊱ *${prefix}map*
+┣⊱ *${prefix}kbbi*
+┣⊱ *${prefix}artinama*
+┣⊱ *${prefix}infogempa*
+┣⊱ *${prefix}quotes*
+┣⊱ *${prefix}cerpen*
+┣⊱ *${prefix}lirik*
+┣⊱ *${prefix}pokemon*
+┣⊱ *${prefix}chord*
+┣⊱ *${prefix}blocklist*
+┣⊱ *${prefix}hidetag*
+┣⊱ *${prefix}wolflogo*
+┣⊱ *${prefix}lionlogo*
+┣⊱ *${prefix}ssweb*
+┣⊱ *${prefix}anjing*
+┣⊱ *${prefix}indohot*
+┣━━❀ *Group* ❀━━
+┣⊱ *${prefix}linkgc*
+┣⊱ *${prefix}tagall*
+┣⊱ *${prefix}add* [+62xx]
+┣⊱ *${prefix}group* [buka/tutup]
+┣⊱ *${prefix}demote* [tag]
+┣⊱ *${prefix}promote* [tag]
+┣⊱ *${prefix}kick*
+┣⊱ *${prefix}listadmin*
+┣⊱ *${prefix}simih* [1/0]
+┣⊱ *${prefix}nsfw* [1/0]
+┣⊱ *${prefix}welcome* [1/0]
+┣━━❀ *Owner bot* ❀━
+┣⊱ *${prefix}clearall*
+┣⊱ *${prefix}block*
+┣⊱ *${prefix}unblock*
+┣⊱ *${prefix}bc*
+┣⊱ *${prefix}leave*
+┣⊱ *${prefix}setpp*
+┣⊱ *${prefix}setprefix*
+┣━━━━━━━━━━━━━━
+┃    *BOT EDIT BY SILAS*
+┗━━━━━━━━━━━━━━
+
+`
+}
+exports.help = help
+
+
+
+  
